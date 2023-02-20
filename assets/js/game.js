996f52167681258b4861bfef49bca114f33f623b
Dary <dady281100@gmail.com>
2023-02-19 23:34:38 -0600

    Adding the first funciton to the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
-alert("This is an alert!, JavaScript is running!")
+alert("This is an alert!, JavaScript is running!");
+
+// This creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
